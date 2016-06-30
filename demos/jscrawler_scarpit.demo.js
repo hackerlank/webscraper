2d8ecd9277c94427e119cf480ac0cb1c13b3b960
jet <jetlyu521@yahoo.com>
2016-06-30 17:29:36 +0800

    minor modify

--- demos/jscrawler_scarpit.demo.js
+++ demos/jscrawler_scarpit.demo.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 var ScrapeIt = require("scrape-it");
 var stringTool = require('../toolkits/stringtool');
 
-fs.writeFileSync('email.csv', 'Begin \r\n', { encoding: 'utf-8' });
+fs.writeFileSync('email_abof.csv', 'Begin \r\n', { encoding: 'utf-8' });
 
 /**
  * Simple crawler. Callback to process data
@@ -19,7 +19,7 @@ var options = {
 }
 
 // crawl
-new Crawler().configure(options).crawl("http://www.zaful.com/", function onSuccess(page) {
+new Crawler().configure(options).crawl("http://www.abof.com/", function onSuccess(page) {
     console.log('Start processing ' + page.url);
     var url = page.url;
 
@@ -31,7 +31,7 @@ new Crawler().configure(options).crawl("http://www.zaful.com/", function onSucce
         if (r) {
             console.log('Email fetched ... ');
             for(var em in r) {
-                fs.appendFileSync('email.csv', '"' + r[em] + '","' + url + '"', { encoding: 'utf-8' });
+                fs.appendFileSync('email_abof.csv', '"' + r[em] + '","' + url + '"\r\n', { encoding: 'utf-8' });
             }
             
         }
