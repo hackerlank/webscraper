1c5ebe0ff1df593239c39b227893e8c05a1f97a5
jet <jetlyu521@yahoo.com>
2016-07-29 14:00:46 +0800

    useless ew

--- private/ExcelWriter.js
+++ private/ExcelWriter.js
@@ -1,5 +1,27 @@
 /// <reference path="../include.d.ts" />
 
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used *//** This wrapper will be no longer used */
+/** This wrapper will be no longer used *//** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used *//** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+/** This wrapper will be no longer used */
+
 var fs = require("fs");
 var xlsx = require('node-xlsx');
 
@@ -7,46 +29,45 @@ var xlsx = require('node-xlsx');
 /**
  * @param Saving file
  * @columns columns name array e.g ["ColumnA", "ColumnB", "ColumnC"]
+ * @sheetName The first sheet's name
  * 
  * Wrapper for writing excel
  */
-var ExcelWriter = function(file, columns, sheetName) {
-    this._sheetName = sheetName;
-    this._file = file;
-    this._data = [];
-    this._buffer = null;
-    this._columns = columns;
-    this._data.push(columns);
+var ExcelWriter = function (file, sheetName) {
+    this.sheets = [sheetName];
+    this.file = file;
+    this.current = sheetName;
 }
+
+/**
+ * Decide which ...
+ * No need to wrap xlsx.
+ */
+ExcelWriter.prototype.switchTo = function(sheetName) {
+
+};
 
 /**
  * @param row  text array of appending row e.g ["cell1", "cell2", "cell3"]
  */
-ExcelWriter.prototype.appendRow = function(row) {
-    this._data.push(row);
+ExcelWriter.prototype.appendRow = function (row) {
+    
 }
 
 /**
  * @return buffer
  */
-ExcelWriter.prototype.build = function() {
-    this._buffer = xlsx.build([{name : this._sheetName, data :  this._data}]);
-    fs.writeFile(this._file, this._buffer, function(err) {
+ExcelWriter.prototype.build = function () {
+    this._buffer = xlsx.build([{ name: this._sheetName, data: this._data }]);
+    fs.writeFile(this._file, this._buffer, function (err) {
         console.error('Unable to write file ' + this._file);
     });
-    // fs.open(this._file , 'w+', function(err, fd) {
-    //     fs.write(fd, this._buffer, function(err, num, str) {
-    //         if(err) {
-    //             console.error(err);
-    //         }
-    //     });
-    // });
 }
 
 /**
  * output current status
  */
-ExcelWriter.prototype.log = function() {
+ExcelWriter.prototype.log = function () {
     console.log(this._data);
 }
 
