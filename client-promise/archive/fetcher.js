daf084fc680919ff0c2373569fb3065fb3f09a81
unknown <>
2016-10-20 00:14:43 +0800

    Basically done

--- client-promise/archive/fetcher.js
+++ client-promise/archive/fetcher.js
@@ -11,12 +11,16 @@ var sheet = { name: 'result', data: [] };
 sheet.data.push(columns);
 var rows = sheet.data;
 
-var timeout = 3000;
+var timeout = 1500;
+
+var optFolder = 'articles/';
 
 /** compose url by yourself */
-var urls = fs.readFileSync('lists.txt').toString().split('\r\n');
+var urls = fs.readFileSync('articlelinks.txt').toString().split('\r\n');
+var needToRework = [];
 
-urls = urls.slice(0, 50);
+
+// urls = urls.slice(0, 5);
 
 /**  Use the for loop if neccessary
 for (var i = 1; i < 100; i++) {
@@ -33,17 +37,27 @@ var printToExcel = function () {
     console.log('Everything was done successfully');
 }
 
-Promise.all(Promise.map(urls, singleRequest, { concurrency: 5 })).then(printToExcel);
+var sayAllDone = function () {
+    console.log('Everything was done');
+}
+
+var writeNeedToRework = function () {
+    fs.writeFileSync('NeedToRework.txt', needToRework);
+}
+
+Promise.all(Promise.map(urls, singleRequest, { concurrency: 5 })).then(sayAllDone).then(writeNeedToRework);
 
 
 /** Single Req */
 function singleRequest(url) {
+    var result = {};
+    var fileName = optFolder + url.split('/')[url.split('/').length - 2] + '.json';
     var options = {
         method: 'GET',
         uri: url,
         headers: {
         },
-        gzip: true
+        gzip: true,
     };
     return rp(options)
         .then(function (body) {
@@ -53,16 +67,69 @@ function singleRequest(url) {
             // text, tags, img, category, name of author, signatures with comments 
             var text = $('#petition_statement').text();
             var author = $('#petition_author a').text();
-            rows.push([title, text, author]);
+            result['title'] = title;
+            result['text'] = text;
+            result['author'] = author;
+            result['signatures'] = [];
+            var links = [];
+            $('#sig_links div a').each(function (index, element) {
+                links.push('http://web.archive.org' + $(this).attr('href'));
+            });
+
+            // fs.writeFileSync(fileName, JSON.stringify(result));
+            // result = null;
+            // rows.push([title, text, author]);
             return new Promise(function (res, rej) {
                 setTimeout(function () {
-                    res(url);
+                    res(links);
                 }, timeout);
             });
 
-        }).then(function (url) {
-            console.log(url + ' was done');
+        }).then(function (links) {
+            console.log(links.length + ' signatures fetched, start scraping');
+
+            function scrapeSignature(link) {
+                console.log('Start working on ' + link);
+                var comments = [];
+                var sig_entity = { link: link, comments: comments };
+                var options = {
+                    method: 'GET',
+                    uri: link,
+                    headers: {
+                    },
+                    gzip: true,
+                    timeout : 25000
+                };
+                return rp(options)
+                    .then(function (body) {
+                        var $ = cheerio.load(body);
+                        $('.sig_comment').each(function (index, element) {
+                            comments.push($(this).text());
+                        });
+                        result.signatures.push(sig_entity);
+                        return new Promise(function (res, rej) {
+                            setTimeout(function () {
+                                res(result);
+                            }, timeout);
+                        });
+                    }).then(function (result) {
+                        console.log(link + ' was done successfully');
+                    }).catch(function (err) {
+                        needToRework.push(url + '-' + link);
+                        fs.appendFileSync('error.txt', err + ' - ' + 　link + 　'\r\n');
+                        // return err;
+                    });
+            }
+
+            return Promise.map(links, scrapeSignature, { concurrency: 5 });
+        }).then(function () {
+            fs.writeFileSync(fileName, JSON.stringify(result));
+            result = null;
         }).catch(function (err) {
-            //handle errors
+            if (needToRework.indexOf(url) === -1) {
+                needToRework.push(url + '\r\n');
+            }
+            fs.writeFileSync(fileName, JSON.stringify(result));
+            result = null;
         });
 }
