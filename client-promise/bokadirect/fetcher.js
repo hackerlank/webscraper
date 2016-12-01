c09f3ff3e33d783bf19636657fb2bf6dd6cf8e9a
jet <jetlyu521@yahoo.com>
2016-12-01 13:23:30 +0800

    commit bokadirekt

--- client-promise/bokadirect/fetcher.js
+++ client-promise/bokadirect/fetcher.js
@@ -6,9 +6,10 @@ var cheerio = require('cheerio');
 var ew = require('node-xlsx');
 var fs = require('fs');
 
-var FACEBOOK_REGEX = /(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-\.]*)?/;
+rp = rp.defaults({timeout : 30000});
+var FACEBOOK_REGEX = /(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/(?:(?:\w)*#!\/)?(?:pages\/)?(?:[?\w\-]*\/)?(?:profile.php\?id=(?=\d.*))?([\w\-\.\%]*)?/;
 
-var columns = ["Name", "Phone", "Website Domain", "Email", "Address", "Postal code", "City", "Facebook Link"];
+var columns = ["Name", "Phone1", "Phone2", "Website Domain", "Email", "Address", "Postal code", "City", "Facebook Link"];
 var sheet = { name: 'result', data: [] };
 sheet.data.push(columns);
 var rows = sheet.data;
@@ -17,12 +18,12 @@ var timeout = 1500;
 
 /** compose url by yourself */
 var urls = fs.readFileSync('hrefs.txt').toString().split('\r\n');
-urls = urls.slice(0, 100);
+// urls = urls.slice(0, 30);
 
 /** function to print excel */
 var printToExcel = function () {
     var buffer = ew.build([sheet]);
-    fs.writeFileSync('bokadirekt.xlsx', buffer);
+    fs.writeFileSync('bokadirekt_update.xlsx', buffer);
     console.log('Excel printed');
 }
 
@@ -47,15 +48,17 @@ function singleRequest(url) {
             var $ = cheerio.load(body);
             //process html via cheerio
             var descriptionText = $('#ctl00_MainContent_ctl07__pnlUserContent').text();
-            var facebookLink = descriptionText.match(FACEBOOK_REGEX) ? descriptionText.match(FACEBOOK_REGEX) : '';
+            var facebookLink = descriptionText.match(FACEBOOK_REGEX) ? descriptionText.match(FACEBOOK_REGEX)[0] : '';
             var name = $('*[itemprop="name"]').text();
             var phone = $('*[itemprop="telephone"]').text();
+            var phone1 = phone.split(' - ')[0];
+            var phone2 = phone.split(' - ')[1] ? phone.split(' - ')[1] : '';
             var websiteDomain = $('*[itemprop="url"] a').text();
             var email = $('*[itemprop="email"] a').text();
-            var address =$('*[itemprop="streetAddress"]').text().trim();
-            var postal =$('*[itemprop="postalCode"]').text().trim();
-            var city =$('*[itemprop="addressLocality"]').text().trim();
-            rows.push([name, phone, websiteDomain, email, address, postal, city, facebookLink]);
+            var address =$('*[itemprop="streetAddress"]').eq(0).text().trim();
+            var postal =$('*[itemprop="postalCode"]').eq(0).text().trim().replace(/\s+/g, '');
+            var city =$('*[itemprop="addressLocality"]').eq(0).text().trim();
+            rows.push([name, phone1, phone2, websiteDomain, email, address, postal, city, facebookLink]);
             return new Promise(function (res, rej) {
                 setTimeout(function () {
                     res(url);
